bb834703e9a42a413b2923dc4d1a234833974e8d
RilDev <c.public@tutanota.com>
2020-08-15 22:09:08 +0200

    improvement(src): create home-view component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import '@rildev/minimal-css/style.css';
-import LatestPostsList from './components/latest-posts-list/latest-posts-list';
-import LatestVideosList from './components/latest-videos-list/latest-videos-list';
-import Layout from './components/layout/layout';
+import HomeView from './views/home-view/home-view'
 import styled from 'styled-components';
 
 const AppWrapper = styled.div``;
 
 function App() {
   return (
-    <Layout>
-      <h2>Latest Posts</h2>
-      <LatestPostsList />
-      <h2>Latest Videos</h2>
-      <LatestVideosList />
-    </Layout>
+    <>
+      <HomeView/>
+    </>
   );
 }
 
