401217b54021d3ec7021f58afcb181e75eeac539
RilDev <c.public@tutanota.com>
2020-08-17 23:35:52 +0200

    improvement(src/hooks): add scrollToTop hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import '@rildev/stylize/style.css';
+import ScrollToTop from './hooks/scroll-to-top';
+import '@rildev/stylized/style.css';
 import HomeView from './views/home-view/home-view';
 import AboutView from './views/about-view/about-view';
+import BlogView from './views/blog-view/blog-view';
+import PostView from './views/post-view/post-view';
 import styled from 'styled-components';
 
 const AppWrapper = styled.div``;
@@ -10,9 +13,16 @@ const AppWrapper = styled.div``;
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Switch>
         <Route path="/about">
           <AboutView />
+        </Route>
+        <Route path="/blog">
+          <BlogView />
+        </Route>
+        <Route path="/post/:slug">
+          <PostView />
         </Route>
         <Route path="/">
           <HomeView />
