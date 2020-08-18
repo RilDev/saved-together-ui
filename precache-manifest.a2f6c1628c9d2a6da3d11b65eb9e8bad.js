3056f9dd3e004f3c23f8603187f7ba7a5ed2403f
RilDev <c.public@tutanota.com>
2020-08-18 12:30:19 +0200

    Updates

---
+++ precache-manifest.a2f6c1628c9d2a6da3d11b65eb9e8bad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5e22f01703c3319e7356422a7bce64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "630149e6017c9eef3fa7",
+    "url": "/static/css/2.da4586f2.chunk.css"
+  },
+  {
+    "revision": "630149e6017c9eef3fa7",
+    "url": "/static/js/2.28f52e22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.28f52e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e66c4ebf6d55fa9ed097",
+    "url": "/static/js/main.282e84c6.chunk.js"
+  },
+  {
+    "revision": "e013172b2f49ceab74f9",
+    "url": "/static/js/runtime-main.57724747.js"
+  }
+]);
